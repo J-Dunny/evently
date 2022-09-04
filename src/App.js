0cad23d2f500afc88446219f04c611899617c13f
Jake Dunafon <jlad795@gmail.com>
2022-09-04 14:58:14 -0700

    Add getEvents

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import { useState, useEffect } from 'react'
 
 function App() {
 
-
+useEffect(() => {
+  getEvents().then(data => console.log(data))
+}, [])
 
 
 
   return (
     <div className="App">
-      <
+      <h1 className='title'>Evently</h1>
     </div>
   );
 }
