4bab35a67a576b33d3b6c304b7af59e2f6cdd196
Jake Dunafon <jlad795@gmail.com>
2022-09-05 11:40:05 -0700

    Add fetch calls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,13 +1,5 @@
-const getEvents = () => {
-    // const options = {
-    //     method: 'GET',
-    //     headers: {
-    //         'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-    //         'X-RapidAPI-Key': '0db63607a1mshb7058632ce290c3p10e57ajsn0d36c557c666'
-    //     }
-    // };
-
-    const fetchCall = fetch('https://www.eventbriteapi.com/v3/users/me/?token=JXJB65RSJCRAKPWOM5QK', options)
+const getUser = () => {
+    const fetchCall = fetch('https://www.eventbriteapi.com/v3/users/me/?token=JXJB65RSJCRAKPWOM5QK')
         .then(response => {
             if (response.status === 404) {
                 throw new Error("404: Not Found")
@@ -19,4 +11,77 @@ const getEvents = () => {
     return fetchCall
 }
 
-export { getEvents }
+const getEvents = () => {
+    const options = {
+        method: 'GET',
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        }
+    }
+    const fetchCall = fetch('https://www.eventbriteapi.com/v3/events', options)
+        .then(response => {
+            if (response.status === 404) {
+                throw new Error("404: Not Found")
+            } else if (response.status === 500) {
+                throw new Error("500: Server is having issues")
+            }
+            return response.json()
+        })
+    return fetchCall
+}
+
+
+const getAllEvents = () => {
+    const encodedParams = new URLSearchParams();
+    encodedParams.append("apiKey", "<REQUIRED>");   
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+		    'X-RapidAPI-Key': '0db63607a1mshb7058632ce290c3p10e57ajsn0d36c557c666',
+		    'X-RapidAPI-Host': 'Ticketmasterstefan-skliarovV1.p.rapidapi.com',
+            'Access-Control-Allow-Origin': '*'
+        },
+        body: encodedParams
+    }
+    const fetchCall = fetch('https://ticketmasterstefan-skliarovv1.p.rapidapi.com/searchEvents', options)
+        .then(response => {
+            if (response.status === 404) {
+                throw new Error("404: Not Found")
+            } else if (response.status === 500) {
+                throw new Error("500: Server is having issues")
+            }
+            return response.json()
+        })
+    return fetchCall
+}
+
+// const getEvents = () => {
+// const encodedParams = new URLSearchParams();
+// encodedParams.append("apiKey", "<REQUIRED>");
+
+// const options = {
+// 	method: 'POST',
+// 	headers: {
+// 		'content-type': 'application/x-www-form-urlencoded',
+// 		'X-RapidAPI-Key': '0db63607a1mshb7058632ce290c3p10e57ajsn0d36c557c666',
+// 		'X-RapidAPI-Host': 'Ticketmasterstefan-skliarovV1.p.rapidapi.com'
+// 	},
+// 	body: encodedParams
+// };
+
+// const fetchCall = fetch('https://ticketmasterstefan-skliarovv1.p.rapidapi.com/searchEvents', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
+// return fetchCall
+// }
+
+export { getUser, getEvents, getAllEvents }
+
+
+
+// OAuth token: JXJB65RSJCRAKPWOM5QK
+// User ID: 157524127868
+// App key: KROZYOJLML6XYZDSJC
